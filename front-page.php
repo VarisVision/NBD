ae25c80010f186048a961375232a78a94ef7ad1b
Jakub Varinsky <hello@varo.design>
2023-10-31 08:22:10 +1100

    frontpage structure edit

--- front-page.php
+++ front-page.php
@@ -1,16 +1,18 @@
 <?php 
 require_once('components/category-products.php');
 require_once('components/hero-banner.php');
-$imageFirst = get_field('first_category_image');
+$imageCategoryFirst = get_field('first_category_image');
+$imageCategorySecond = get_field('second_category_image');
 
 get_header();
 ?>
 
 <main class="no-bad-days">
     <?php 
-        heroBanner($imageFirst['sizes']['large'], $imageFirst['alt']);
+        heroBanner($imageCategoryFirst['sizes']['large'], $imageCategoryFirst['alt']);
     ?>
-    <section class="nbd-front-page__section">
+
+    <section class="nbd-section">
         <div class="nbd-four-column-products">
             <?php    
                 categoryProducts('t-shirt');
@@ -18,20 +20,18 @@ get_header();
         </div>
     </section>
 
-    <section class="nbd-front-page__section">
+    <?php 
+        heroBanner($imageCategorySecond['sizes']['large'], $imageCategorySecond['alt']);
+    ?>
+
+    <section class="nbd-section">
         <div class="nbd-four-column-products">
             <?php    
                 categoryProducts('hoodie');
             ?>
         </div>
     </section>
-
-
-
 </main>
-
-
-
 
 <?php
 get_footer();
