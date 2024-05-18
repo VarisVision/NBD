7042683d97779a7d77a2c867e990bbfb791971bc
Jakub Varinsky <hello@varo.design>
2024-05-18 15:00:34 +1000

    hero banner blurry images when lower size

--- components/hero-banner.php
+++ components/hero-banner.php
@@ -1,8 +1,5 @@
 <?php
 function heroBanner($imageData, $imageText) {
-    $mobile = isset($imageData['sizes']['mobile']) ? $imageData['sizes']['mobile'] : '';
-    $tablet = isset($imageData['sizes']['tablet']) ? $imageData['sizes']['tablet'] : '';
-    $laptop = isset($imageData['sizes']['laptop']) ? $imageData['sizes']['laptop'] : '';
     $desktop = isset($imageData['url']) ? $imageData['url'] : '';
 
     $altText = isset($imageData['alt']) ? $imageData['alt'] : '';
@@ -10,11 +7,7 @@ function heroBanner($imageData, $imageText) {
     echo 
         '<div class="nbdc-hero">
             <picture>
-                <source srcset="'. $desktop .'" media="(min-width: 1440px)">
-                <source srcset="'. $laptop .'" media="(min-width: 990px)">
-                <source srcset="'. $tablet .'" media="(min-width: 640px)">
-                <source srcset="'. $mobile .'" media="(min-width: 0px)">
-                <img src="'. $laptop .'" loading="lazy" alt="'. $altText .'"/>
+                <img src="'. $desktop .'" loading="lazy" alt="'. $altText .'"/>
             </picture>
             <div class="nbdc-hero__title">
                 <h1>'. $imageText .'</h1>
